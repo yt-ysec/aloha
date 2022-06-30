a92cfcd94303b40c1408d2282278da0d77c0217d
Yoshino Taichi <taichi_yoshino@yse-c.net>
2022-06-30 11:31:22 +0900

    update 20220630.py

---
+++ kadai/20220630.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 101)
+print(x)
+
+y = np.exp(-x**2 / 2) / np.sqrt(2 * np.pi)
+print(y)
+
+plt.plot(x, y)
+plt.show()
+
